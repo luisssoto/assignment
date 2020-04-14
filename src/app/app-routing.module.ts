65d455a15e9cbac8d3e1478c7add7bf70abdf1bf
Luis Soto <luis.sotoalva@gmail.com>
2020-04-14 06:59:44 -0400

    three out of four reqs working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 
 const routes: Routes = [];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  todoArray = [];
+  todo;
+
+  addTodo(value){
+    if(value !== ""){
+      this.todoArray.push(value)
+    } else{
+      alert('Field required **')
+      console.log(this.todoArray);
+    }
+    
+  }
+
+  deleteItem(todo){
+    for(let i =0; i <= this.todoArray.length; i++){
+      if(todo == this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+
+  todoSubmit(value:any){
+    if(value !== ""){
+      this.todoArray.push(value.todo)
+      console.log(this.todoArray);
+    }else{
+      alert('Field required **')
+    }
+  }
 }
